3ffb9b206279e15f57a99cf2d5366f513313e855
Hiroki Osame <hiroki.osame@gmail.com>
2024-11-09 11:47:17 +0900

    remove other packages

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,17 @@
-const { getJestProjects } = require('@nx/jest');
-
+/* eslint-disable */
 export default {
-  projects: getJestProjects(),
+  displayName: 'nx-remotecache-s3',
+  preset: '../../jest.preset.js',
+  globals: {},
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/nx-remotecache-s3',
 };
--- jest.preset.js
+++
@@ -1,8 +0,0 @@
-const nxPreset = require('@nx/jest/preset').default;
-
-module.exports = {
-  ...nxPreset,
-  collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/**/*.{js,jsx,ts,tsx}'],
-  coverageReporters: ['json'],
-};
